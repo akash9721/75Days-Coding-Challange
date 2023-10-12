79b3309cf0ceff8b7db5260437243fb6c306ebab
Akash kumar <akashkumar972177@gmail.com>
2023-10-12 16:30:00 -0700

    day 14 add list at given place

--- src/com/java/Linkedlist/SingleLinkedlist.java
+++ src/com/java/Linkedlist/SingleLinkedlist.java
@@ -15,61 +15,87 @@ class ListNode {
 	public ListNode(int data, ListNode next) {
 		this.data = data;
 		this.next = next;
-	}	
+	}
 }
 
 public class SingleLinkedlist {
 
 	private static ListNode insertAtFirst(ListNode head, int data) {
-		ListNode node=head;
-		ListNode node1=new ListNode(data);
-		if(node==null) {
+		ListNode node = head;
+		ListNode node1 = new ListNode(data);
+		if (node == null) {
 			return node1;
 		}
-		node1.next=node;
-		node=node1;
+		node1.next = node;
+		node = node1;
 		return node;
 	}
-	
+
 	private static ListNode insertAtLast(ListNode head, int data) {
-		ListNode node=head;
-		ListNode nodex=new ListNode(data);
-		if(node==null) {
+		ListNode node = head;
+		ListNode nodex = new ListNode(data);
+		if (node == null) {
 			return nodex;
 		}
-		while(node.next!=null) {
-			node=node.next;
+		while (node.next != null) {
+			node = node.next;
 		}
-		node.next=nodex;
+		node.next = nodex;
 		return head;
 	}
-	
+
+	private static ListNode insertAtPosition(ListNode head, int data, int n) {
+		ListNode node = head;
+		ListNode node1 = new ListNode(data);
+		if (n < 1) {
+			System.out.println("Invalid position");
+		} else if (n == 1) {
+			if (node == null) {
+				return node1;
+			}
+			node1.next = node;
+			node = node1;
+			return node;
+		}
+		else {
+			for(int i=1;i<n-1;i++) {
+				node=node.next;
+			}
+			node1.next=node.next;
+			node.next=node1;
+		}
+		return head;
+	}
+
 	private static void printList(ListNode head) {
-		ListNode node=head;
-		while(node!=null) {
-			System.out.print(node.data+" ");
-			node=node.next;
+		ListNode node = head;
+		while (node != null) {
+			System.out.print(node.data + " ");
+			node = node.next;
 		}
 	}
-	
+
 	public static void main(String[] args) {
-      ListNode head=new ListNode(10);
-      head.next=new ListNode(3);
-      head.next.next=new ListNode(30);
-      head.next.next.next=new ListNode(21);
-      head.next.next.next.next=new ListNode(5);
-      System.out.println("List traversal is");
-      printList(head);
-      
-      System.out.println();
-      System.out.println("insert at first place");
-      ListNode node=insertAtFirst(head,100);
-      printList(node);
-      
-      System.out.println();
-      System.out.println("insert at last place");
-      ListNode node1=insertAtLast(node, 500);
-      printList(node1);
+		ListNode head = new ListNode(10);
+		head.next = new ListNode(3);
+		head.next.next = new ListNode(30);
+		head.next.next.next = new ListNode(21);
+		head.next.next.next.next = new ListNode(5);
+		System.out.println(" List traversal is");
+		printList(head);
+
+		System.out.println("\ninsert at first place");
+		ListNode node = insertAtFirst(head, 100);
+		printList(node);
+
+		System.out.println("\ninsert at last place");
+		ListNode node1 = insertAtLast(node, 500);
+		printList(node1);
+
+		System.out.println("\ninsert at given position");
+		ListNode node2 = insertAtPosition(head, 999, 3);
+		printList(node2);
+
 	}
 
 }
