01ecc8576ce2d4b93172d9440e2fab3e18886d4b
Akash kumar <akashkumar972177@gmail.com>
2023-10-18 14:13:00 -0700

    day 20

--- src/com/java/Bitwise/EvenOddNo.java
+++ src/com/java/Bitwise/EvenOddNo.java
@@ -13,11 +13,21 @@ public class EvenOddNo {
 		}
 	}
 
+	private static void check2(int n) {
+		if((n&1)==0) {
+			System.out.println("no is even");
+		}
+		else {
+			System.out.println("no is odd");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter a number");
 		int n = sc.nextInt();
 		check(n);
+		check2(n);
 	}
 
 }
