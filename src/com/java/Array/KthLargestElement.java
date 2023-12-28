86405dbf103b2a79eb3763b00d49d2cf4252eddf
Akash kumar <akashkumar972177@gmail.com>
2023-12-28 12:24:52 +0530

    day 13

---
+++ src/com/java/Array/KthLargestElement.java
@@ -0,0 +1,24 @@
+package com.java.Array;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+	private static void maxHeap(int[] a,int k) {
+		PriorityQueue<Integer> pq=new PriorityQueue<>();
+		for(int i=0;i<a.length;i++) {
+			pq.add(a[i]);
+			
+			if(pq.size()>k)
+				pq.poll();
+		}
+		System.out.println(k+"th largest element is :"+pq.peek());
+	}
+	
+	public static void main(String[] args) {
+		int a[]= {10,50,20,1,15,9};
+		int k=4;
+		maxHeap(a,k);
+	}
+}
